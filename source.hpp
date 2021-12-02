f92128502df34ac0858eed533bab90d7dd33e2e6
Aviram Shubeili <aviramshovali@gmail.com>
2021-12-02 10:13:32 +0200

    rules of daniel

--- source.hpp
+++ source.hpp
@@ -16,16 +16,31 @@ typedef enum binoptype {
 };
 
 struct Node {
+    
     virtual std::string prettyPrint();
 };
 
 class Relop : public Node {
     enum reloptype type;
-    Relop(enum reloptype t) : type(t) {}
+    Relop(std::string value) {
+        if(value == "<") type = SMALLER;
+        else if(value == ">") type = BIGGER;
+        else if(value == ">=") type = BIGGER_EQUALS;
+        else if(value == "<=") type = SMALLER_EQUALS;
+        else if(value == "==") type = EQUALS;
+        else if(value == "!=") type = NOT_EQUALS;
+        else throw std::invalid_argument("wrong Relop");
+    }
 };
 class Binop : public Node {
     enum binoptype type;
-    Binop(enum binoptype t) : type(t) {}
+    Binop(std::string value) {
+        if (value == "+") type = PLUS;
+        else if (value == "-") type = MINUS;
+        else if (value == "*") type = MUL;
+        else if (value == "/") type = DIV;
+        else throw std::invalid_argument("wrong Binop");
+    }
 };
 class ID : public Node {
     std::string value;
@@ -37,8 +52,8 @@ private:
     std::string value;
 public:
     Num(std::string text) : value(text) {}
-    int getNumber() {
-        return std::stoi(value);
+    long long getNumber() {
+        return std::stoll(value);
     }
 };
 
